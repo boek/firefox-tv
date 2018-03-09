d2c7bbb1663cfb5f635bc620457e6918496e7aaf
Jeff Boek <jeff@jeffboek.com>
2018-03-09 09:32:14 -0800

    foobar

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -5,17 +5,16 @@
 package org.mozilla.focus.browser.cursor
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RadialGradient
-import android.graphics.Shader
+import android.graphics.*
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.support.annotation.UiThread
+import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
-import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.cursor_view.view.*
 import org.mozilla.focus.R
 import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
@@ -30,19 +29,18 @@ private val HIDE_AFTER_MILLIS = TimeUnit.SECONDS.toMillis(3)
  * A drawn Cursor: see [CursorViewModel] for responding to keys and setting position.
  * The cursor will hide itself when it hasn't received a location update recently.
  */
-class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class CursorView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
     private val hideHandler = CursorHideHandler(this)
 
     // The radius is half the size of the container its drawn in.
     private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
-    private val paint = Paint().apply {
-        style = Paint.Style.FILL
-        alpha = CURSOR_ALPHA
-        isAntiAlias = true
-        shader = RadialGradient(radius, radius, /* radius */ radius,
-                ContextCompat.getColor(context, R.color.teal50), ContextCompat.getColor(context, R.color.photonBlue50),
-                Shader.TileMode.CLAMP)
+
+    init {
+        inflate(context, R.layout.cursor_view, this)
+        cursor_fg.alpha = 0f
+        cursor_fg.setColorFilter(Color.WHITE, PorterDuff.Mode.ADD)
+        cursor_bg.setColorFilter(Color.WHITE, PorterDuff.Mode.LIGHTEN)
     }
 
     @UiThread
@@ -53,16 +51,6 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
         setMaxVisibility()
         resetCountdown()
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        // We draw the cursor in the top-left corner, offset so the circle isn't clipped by its
-        // parent. The translationX/Y properties will draw it into the desired position on the
-        // screen. We do it this way to avoid allocating new RadialGradients, which are
-        // constructed in relation to the current position of the circle.
-        canvas.drawCircle(radius, radius, radius, paint)
     }
 
     fun cancelUpdates() {
